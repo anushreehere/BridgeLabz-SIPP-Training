e288f3ab0e1a5093b7de2a381c00d3e9ee1cfeab
anushreehere <122143000+anushreehere@users.noreply.github.com>
2025-08-27 16:51:34 +0530

    Removed folders from this branch

--- linearAndBinarySearch/ReverseStringSB.java
+++
@@ -1,8 +0,0 @@
-public class ReverseStringSB {
-    public static void main(String[] args) {
-        String str = "hello";
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        System.out.println("Reversed string: " + sb.toString());
-    }
-}
